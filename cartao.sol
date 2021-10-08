c46035801b3555a3da6879e1c1d0e81424608a8c
Jumaravaz <92129734+Jumaravaz@users.noreply.github.com>
2021-10-07 21:41:11 -0300

    Criação do cartao.sol

---
+++ cartao.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.9;
+
+contract CartaoDeVacinacao {
+     // Autor: Jumara Vaz
+     string constant public nome = "Jumara Vaz";
+     string public unidade = "Horto";
+     uint public numero_CNES = 1 ;
+     string public data = "01/10/2021";
+     uint private lote = 100;
+     string constant private fabricante = "Janssen";
+     bool public imunizado = false;
+     string public vacinador = "Antonio";
+     address private wallet = 0x4e3848F10F6fC3Ba91E69cb80287cc48644Cd6c6;
+
+    
+}
