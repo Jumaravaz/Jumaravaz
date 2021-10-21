ae132348e656a634aa7c5b83af0e6107606c0748
Jumaravaz <92129734+Jumaravaz@users.noreply.github.com>
2021-10-21 19:09:27 -0300

    Create parcelamento.sol

---
+++ parcelamento.sol
@@ -0,0 +1,33 @@
+//contrato para parcelamento de financiamento de veiculo
+
+pragma solidity 0.8.9;
+
+contract parcelamentodeveiculo 
+{
+    // Autor: Jumara Vaz
+     string public comprador;
+     string public renavan;
+     string public consercionaria;
+     string public marca;
+     string public modelo;
+     
+     uint public valortotal;
+     uint public qtdparcela;
+     uint public valordaparcela;
+     
+     constructor (
+         string memory _comprador,
+         string memory _renavan,
+         string memory _consercionaria,
+         string memory _marca,
+         string memory _modelo)
+    
+    function calculovalordaparcela (uint _valordaparcela) public returns (uint, string memory)
+    {
+        _valordaparcela = valortotal / qtdparcela;
+        valordaparcela = _valordaparcela;
+        return(valordaparcela, "valor da parcela");
+    } 
+    
+    
+}
